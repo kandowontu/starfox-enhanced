627f3ab530f48e03ee74555926a4002332ee1c5e
kandowontu <kandowontu@me.com>
2026-09-08 21:40:25 -0400

    Make localized menu accents portable to MSVC source encodings

--- src/render/scaled_text_renderer.cpp
+++ src/render/scaled_text_renderer.cpp
@@ -15,7 +15,7 @@ namespace starfox::render {
 namespace {
 struct MenuLatinGlyph { char32_t base; char32_t accent; };
 MenuLatinGlyph menu_latin_glyph(char32_t code) {
-    constexpr std::u32string_view accented = U"ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïñòóôõöùúûüýÿ";
+    constexpr std::u32string_view accented = U"\u00c0\u00c1\u00c2\u00c3\u00c4\u00c5\u00c7\u00c8\u00c9\u00ca\u00cb\u00cc\u00cd\u00ce\u00cf\u00d1\u00d2\u00d3\u00d4\u00d5\u00d6\u00d9\u00da\u00db\u00dc\u00dd\u00e0\u00e1\u00e2\u00e3\u00e4\u00e5\u00e7\u00e8\u00e9\u00ea\u00eb\u00ec\u00ed\u00ee\u00ef\u00f1\u00f2\u00f3\u00f4\u00f5\u00f6\u00f9\u00fa\u00fb\u00fc\u00fd\u00ff";
     constexpr std::u32string_view bases = U"AAAAAACEEEEIIIINOOOOOUUUUYaaaaaaceeeeiiiinooooouuuuyy";
     constexpr std::u32string_view accents = U"\u0300\u0301\u0302\u0303\u0308\u030a\u0327\u0300\u0301\u0302\u0308\u0300\u0301\u0302\u0308\u0303\u0300\u0301\u0302\u0303\u0308\u0300\u0301\u0302\u0308\u0301\u0300\u0301\u0302\u0303\u0308\u030a\u0327\u0300\u0301\u0302\u0308\u0300\u0301\u0302\u0308\u0303\u0300\u0301\u0302\u0303\u0308\u0300\u0301\u0302\u0308\u0301\u0308";
     static_assert(accented.size() == bases.size() && bases.size() == accents.size());
